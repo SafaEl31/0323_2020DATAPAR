60f8ad35791b64dc0e80937f0fc47315d1ac2ba1
SafaEl31 <safa.eladib@gmail.com>
2020-06-02 15:38:51 +0200

    personal projects in data analysis and machine learning

--- Projects/Investment-project/create_startup_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE startup (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20),
-  description text,
-  founded_date VARCHAR(40),
-  category_list text,
-  total_funding_amout DOUBLE,
-  PRIMARY KEY (id)
-);
